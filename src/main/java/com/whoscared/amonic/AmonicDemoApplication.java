2ba45e342c27ddfc8a2f749997115985fc3485f4
whoscared <coklook1@gmail.com>
2023-01-19 20:20:45 +0300

    Initial commit

---
+++ src/main/java/com/whoscared/amonic/AmonicDemoApplication.java
@@ -0,0 +1,13 @@
+package com.whoscared.amonic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmonicDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmonicDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/whoscared/amonic/AmonicDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whoscared.amonic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmonicDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
